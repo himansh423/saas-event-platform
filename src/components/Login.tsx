45013732aec4ac45e3ab828a808691e6c500be1a
Himanshu Chauhan <himanshuchau423@gmail.com>
2024-12-23 10:32:05 +0530

    initialized project with login page

---
+++ src/components/Login.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (
+    <div className="w-screen h-screen flex">
+      <div className="imageContainer flex-1 min-h-screen bg-black"></div>
+      <div className="credentialContainer min-h-screen w-[500px] bg-yellow-400"></div>
+    </div>
+  );
+};
+
+export default Login;
