649f3073fbdc8742229e522cb95a3ced6495ddf6
Himanshu Chauhan <himanshuchau423@gmail.com>
2025-01-02 10:23:55 +0530

    navbar Ui done except dropdowns

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,20 +19,26 @@ const Navbar = () => {
       <div
         className={`${rowdies1.className} flex items-center gap-12 absolute left-[50%] translate-x-[-50%] text-2xl`}
       >
-        <Link href={"/blog"}>Blog</Link>
-        <p>Events</p>
-        <p>Hackathons</p>
+        <Link href={"/blog"} className="hover:text-[#0c1feb] duration-75">
+          Blog
+        </Link>
+        <Link href={"/"} className="hover:text-[#0c1feb] duration-75">
+          Events
+        </Link>
+        <Link href={"/hackathons"} className="hover:text-[#0c1feb] duration-75">
+          Hackathons
+        </Link>
       </div>
       <div className="flex gap-5 text-3xl items-center">
-        <div className="pt-[1px] mt-[3px]">
+        <div className="pt-[1px] mt-[2px] hover:text-[#0c1feb] duration-75">
           <MdOutlineNotificationsNone />
         </div>
-        <div className="flex gap-2 items-center text-xl p-2 border-dashed border-[1px] rounded-[30px]">
-          <div >
+        <div className="flex gap-2 items-center text-xl p-2 border-dashed group border-[1px] rounded-[30px] hover:shadow-[0_0_0_2px_#0c1feb] duration-100">
+          <div className="mt-[1px]">
             <FaRegUserCircle />
           </div>
           <p>Himanshu</p>
-          <div className="mt-[5px]">
+          <div className="mt-[5px] group-hover:text-[#0c1feb]">
             <IoMdArrowDropdown />
           </div>
         </div>
