a5461ae7d031701a72027da4672c4693e71185f5
Himanshu Chauhan <himanshuchau423@gmail.com>
2025-01-28 11:53:26 +0530

    created get event details API for a specific event page

---
+++ src/app/api/get-event-details/[id]/route.ts
@@ -0,0 +1,40 @@
+import connectToDatabase from "@/library/db";
+import EventAndHackathon from "@/library/Modal/EventsAndHackathonSchema";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = await params;
+    await connectToDatabase();
+    const eventOrHackathon = await EventAndHackathon.findById(id);
+
+    if (!eventOrHackathon) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "event or hackathon not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(eventOrHackathon, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching event or hackathon data:", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Error fetching event or hackathon data",
+      },
+      { status: 500 }
+    );
+  }
+}
