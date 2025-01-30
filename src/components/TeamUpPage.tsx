8be3a43254818b5ea7e504dac7dda0ff42e1b6df
Himanshu Chauhan <himanshuchau423@gmail.com>
2025-01-30 13:03:05 +0530

    created separate team up card component

--- src/components/TeamUpPage.tsx
+++ src/components/TeamUpPage.tsx
@@ -1,5 +1,6 @@
 import { Calendar, Mail, MapPin, Phone } from "lucide-react";
 import { Rowdies } from "next/font/google";
+import TeamUpCard from "./TeamUpCard";
 
 const rowdies1 = Rowdies({
   weight: "700",
@@ -17,63 +18,9 @@ const TeamUpPage = () => {
         </h1>
       </div>
       <div className="w-full px-6 grid grid-cols-3 place-items-center py-20">
-        <div className="w-[467px] h-[367px] bg-[#111111] rounded-md border-[1px] border-[#222] flex flex-col items-start justify-between px-4 py-5">
-          <div className="flex flex-col gap-1">
-            <p
-              className={`${rowdies1.className} text-2xl bg-gradient-to-r from-blue-400 to-[#0c1feb] bg-clip-text text-transparent`}
-            >
-              Delhi Tech Summit Hackathon 2024
-            </p>
-            <p className="text-[14px] text-[#A3A3A3]">
-              Posted by Himanshu Chauhan
-            </p>
-          </div>
-          <div className="text-[#D1D5DB] text-[14px]">
-            <p>
-              Looking for team members skilled in full-stack development and
-              UI/UX design for a 24-hour hackathon focused on AI/ML solutions.
-            </p>
-          </div>
-          <div className="flex  flex-col gap-2">
-            <div className="flex gap-1 items-center text-[14px] text-[#9CA3AF]">
-              <div>
-                <Calendar className="h-4 w-4" />
-              </div>
-              <p>15-16 February 2024</p>
-            </div>
-            <div className="flex gap-1 items-center text-[14px] text-[#9CA3AF]">
-              <div>
-                <MapPin className="h-4 w-4" />
-              </div>
-              <p>New Delhi, India</p>
-            </div>
-            <div className="flex gap-1 items-center text-[14px] text-[#9CA3AF]">
-              <div>
-                <Mail className="h-4 w-4" />
-              </div>
-              <p>himanshuchau423@gmail.com</p>
-            </div>
-            <div className="flex gap-1 items-center text-[14px] text-[#9CA3AF]">
-              <div>
-                <Phone className="h-4 w-4" />
-              </div>
-              <p>+91-8287015235</p>
-            </div>
-          </div>
-          <div className="flex items-center justify-between w-full gap-5">
-          <div className="w-1/2 h-[40px] bg-gradient-to-r from-blue-400 to-[#0c1feb] rounded-sm flex justify-center items-center">
-            <p className={`${rowdies1.className} text-white`}>
-              Apply for Team Up
-            </p>
-          </div>
-          <div className="w-1/2 h-[40px] bg-gradient-to-r from-blue-400 to-[#0c1feb] rounded-sm flex justify-center items-center">
-            <p className={`${rowdies1.className} text-white`}>
-             More Info
-            </p>
-          </div>
-          </div>
-         
-        </div>
+        {[1, 2, 3, 4, 5, 6].map((item) => (
+          <TeamUpCard />
+        ))}
       </div>
     </div>
   );
